8e9c311fc1fe7fea50e669cce5493af7ffe66398
Math <ilovethevikings@yahoo.com>
2026-05-13 01:52:39 -0500

    fix: add missing cn import to GeneralPage

--- app/src/components/ServerTab/GeneralPage.tsx
+++ app/src/components/ServerTab/GeneralPage.tsx
@@ -14,6 +14,7 @@ import { useAutoUpdater } from '@/hooks/useAutoUpdater';
 import { useServerHealth } from '@/lib/hooks/useServer';
 import { usePlatform } from '@/platform/PlatformContext';
 import { useServerStore } from '@/stores/serverStore';
+import { cn } from '@/lib/utils/cn';
 import { LanguageSelect } from './LanguageSelect';
 import { SettingRow, SettingSection } from './SettingRow';
 import { ThemeSelect } from './ThemeSelect';
